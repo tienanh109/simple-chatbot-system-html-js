48290ee02e4af8ca98003d8e9e808ee777e6fe92
BinZFAKE9999 <99895433+BinZFAKE9999@users.noreply.github.com>
2022-12-24 12:21:28 +0700

    Add files via upload

---
+++ system_bot_by_binzfake.js
@@ -0,0 +1,42 @@
+var coll = document.getElementsByClassName("collapsible");
+for (let i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    });
+}
+function getHardResponse(userText) {
+    let botResponse = getBotResponse(userText);
+    let botHtml = '<p class="botText"><span>' + botResponse + '</span></p>';
+    $("#chatbox").append(botHtml);
+}
+function getResponse() {
+    let userText = $("#textInput").val();
+    let userHtml = '<p class="userText"><span>' + userText + '</span></p>';
+    $("#textInput").val("");
+    $("#chatbox").append(userHtml);
+    document.getElementById("chat-bar-bottom").scrollIntoView(true);
+
+    setTimeout(() => {
+        getHardResponse(userText);
+    }, 0)
+
+}
+function buttonSendText(sampleText) {
+    let userHtml = '<p class="userText"><span>' + sampleText + '</span></p>';
+    $("#textInput").val("");
+    $("#chatbox").append(userHtml);
+}
+function sendButton() {
+    getResponse();
+}
+$("#textInput").keypress(function (e) {
+    if (e.which == 13) {
+        getResponse();
+    }
+});
